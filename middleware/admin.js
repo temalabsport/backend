a67a850c37b22678b38c680dbaae04527cb3a3fd
bpetii <biro.peti09@gmail.com>
2018-11-08 11:34:30 +0100

    Admin middleware

---
+++ middleware/admin.js
@@ -0,0 +1,12 @@
+
+module.exports = function (req, res, next) {
+
+    if (req.user.isAdmin === true) {
+
+        next();
+        return;
+    }
+
+    res.status(403).send("Permission denied");
+
+}
