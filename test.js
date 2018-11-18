163cadf9909cef75af1c9f6989593f99ed6c78ff
Bodnár Kevin <bodnar.kevin@gmail.com>
2018-11-18 11:15:50 +0100

    Unit testing

---
+++ test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+
+const passwordHash = require('./utils/passwordHash');
+
+describe('Password Hashing', function () {
+    it('Hashing and checking password', function () {
+        const passwordString = [...Array(16)].map(i=>(~~(Math.random()*36)).toString(36)).join('');
+        const passwordData = passwordHash.generateNew(passwordString);
+        assert.strictEqual(passwordHash.validate(passwordString, passwordData.passwordSalt, passwordData.passwordHash), true);
+    });
+
+    it('Validating bad password', function () {
+        const passwordString = 'aaaaa';
+        const badPasswordString = 'bbbbbbbb';
+        const passwordData = passwordHash.generateNew(passwordString);
+        assert.strictEqual(passwordHash.validate(badPasswordString, passwordData.passwordSalt, passwordData.passwordHash), false);
+    });
+});
