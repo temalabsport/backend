855f65a6baff8411be75825d8e2b59e67509353b
Bodnár Kevin <bodnar.kevin@gmail.com>
2018-12-01 22:38:07 +0100

    CORS fixes for fronted

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const morgan = require('morgan')
 const config = require('config');
 const cron = require('node-cron');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 Sentry.init({ dsn: 'https://2a77f90cdc22481c8031832cf7796259@sentry.io/1330652' });
@@ -24,10 +25,12 @@ app.use(morgan('tiny'));
 app.use(express.static('public'));
 app.use(express.json());
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-});
+const corsOptions = {
+    allowedHeaders: ['x-auth-token', 'Content-Type'],
+    exposedHeaders: ['x-auth-token']
+}
+
+app.use(cors(corsOptions));
 
 app.use('/api/user', user);
 app.use('/api/sports', sports);
